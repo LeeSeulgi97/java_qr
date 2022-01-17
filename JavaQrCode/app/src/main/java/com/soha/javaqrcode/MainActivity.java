52cf20164623c302cb817f79cc6b5bd9d146082a
leeseulgi <dltmfrl@gmail.com>
2022-01-17 16:12:57 +0900

    필요없는 코드 삭제 및 세로모드 인식변환

--- JavaQrCode/app/src/main/java/com/soha/javaqrcode/MainActivity.java
+++ JavaQrCode/app/src/main/java/com/soha/javaqrcode/MainActivity.java
@@ -1,13 +1,8 @@
 package com.soha.javaqrcode;
 
-import android.content.Context;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,21 +17,14 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-//    et.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//        @Override public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-//            if(actionId == EditorInfo.IME_ACTION_SEARCH){
-//                // 키보드 숨기기
-//                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-//                imm.hideSoftInputFromWindow(v.getWindowToken(),0);
-//                return true; }
-//            return false; }
-//    });
     integrator = new IntentIntegrator(this);
     //바코드 안의 텍스트
         integrator.setPrompt("바코드를 사각형 안에 비춰주세요");
          //바코드 인식시 소리 여부
         integrator.setBeepEnabled(false);
         integrator.setBarcodeImageEnabled(true);
+        integrator.setOrientationLocked(true);
+        integrator.setOrientationLocked(false);
         integrator.setCaptureActivity(CaptureActivity.class);
         //바코드 스캐너 시작
         integrator.initiateScan();
@@ -55,8 +43,6 @@ public class MainActivity extends AppCompatActivity{
             if(result.getContents() == null){
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             }else{
-//                //qr코드를 읽어서 EditText에 입력해줍니다.
-//                et.setText(result.getContents());
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_SHORT).show();
                 integrator.initiateScan();
             }
